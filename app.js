e034fe3b57f8f61695b59ede1a40b3fe48333084
Rimantė <rimante.razinskaite@gmail.com>
2021-03-02 13:21:51 +0200

    Add nav toggle

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // NAVBAR:
 
-const burgerIcon = () => {
+const navToggle = () => {
     const burger = document.querySelector('.burger');
+    const navBar = document.querySelector('.nav-bar');
+    const primaryMenu = document.querySelector('.primary-menu');
+    const secondaryMenu = document.querySelector('.secondary-menu');
+    const navCTA = document.querySelector('.nav-cta');
+    
+
     burger.addEventListener('click', () => {
         burger.classList.toggle('open');
+        navBar.classList.toggle('open');
+        primaryMenu.classList.toggle('open');
+        secondaryMenu.classList.toggle('open');
+        navCTA.classList.toggle('open');
     })
 
 }
 
-burgerIcon();
+navToggle();
 
 // $(document).ready(function() {
 //     $('.btn-toggle-nav').click(function() {
