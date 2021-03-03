0f8e195fcd2eea96d79c586af806e397130a1636
Rimantė <rimante.razinskaite@gmail.com>
2021-03-03 15:39:52 +0200

    Format app.js  with Prettier

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 // NAVBAR:
 
 const navToggle = () => {
-    const burger = document.querySelector('.burger');
-    const navBar = document.querySelector('.nav-bar');
-    const primaryMenu = document.querySelector('.primary-menu');
-    const secondaryMenu = document.querySelector('.secondary-menu');
-    const navCTA = document.querySelector('.nav-cta');
-    
+  const burger = document.querySelector(".burger");
+  const navBar = document.querySelector(".nav-bar");
+  const primaryMenu = document.querySelector(".primary-menu");
+  const secondaryMenu = document.querySelector(".secondary-menu");
+  const navCTA = document.querySelector(".nav-cta");
 
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('open');
-        navBar.classList.toggle('open');
-        primaryMenu.classList.toggle('open');
-        secondaryMenu.classList.toggle('open');
-        navCTA.classList.toggle('open');
-    })
-
-}
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("open");
+    navBar.classList.toggle("open");
+    primaryMenu.classList.toggle("open");
+    secondaryMenu.classList.toggle("open");
+    navCTA.classList.toggle("open");
+  });
+};
 
 navToggle();
-
 
 // Masonry Layout for archive pages
 
 let magicGrid = new MagicGrid({
-    container: ".archive-grid",
-    static: true,
-    animate: true,
-    gutter: 40  
-  });
-   
+  container: ".archive-grid",
+  static: true,
+  animate: true,
+  gutter: 40,
+});
+
 magicGrid.listen();
