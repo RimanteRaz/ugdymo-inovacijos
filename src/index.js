1219adce4f7980f6894ccebc3c1682dffd30eb7f
Rimantė <rimante.razinskaite@gmail.com>
2021-03-10 14:49:50 +0200

    Test Webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./main.scss";
+
+const headline = "Welcome to the webpage";
+document.querySelector("h1").innerText = headline;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "source-map",
+  mode: "development",
+};
